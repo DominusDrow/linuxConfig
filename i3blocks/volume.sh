a5ace37533a98bcd7253835d383ae6aadef711c9
DominusDrow <galacticovasquez@gmail.com>
2022-12-20 22:47:36 -0600

    add volume & things

--- i3blocks/volume.sh
+++ i3blocks/volume.sh
@@ -1,29 +1,18 @@
-#!/usr/bin/bash
+_BLOCK_39_(){
+local Vol=$(pactl list sinks | awk '/Volumen: front/{print $5-"%"}')
+local Mute=$(pactl list sinks | awk '/Silencio: /')
 
-# script per al volum sense utilitzar "amixer"
-# utilitza "pactl"
-
-case $BLOCK_BUTTON in
-    1) pactl set-sink-mute @DEFAULT_SINK@ toggle ;;
-esac
-
-# això mostra el volum actual
-function vol() {
-	pactl list sinks \
-	| grep '^[[:space:]]Volume:'\
-	| head -n $(( $SINK + 1 )) \
-	| tail -n 1 \
-	| sed -e 's,.* \([0-9][0-9]*\)%.*,\1,'
+if [ "$Mute" = "	Silencio: sí" ];then
+    echo -e " --"
+else
+    if [ "$Vol" -gt "60" ];then
+        echo -e " $Vol%"
+    elif [ "$Vol" -gt "30" ];then
+        echo -e " $Vol%"
+    elif [ "$Vol" -lt "30" ];then
+        echo -e " $Vol%"
+    fi
+fi
 }
 
-# primer comprovam si està mutejat
-if [ "$(pacmd list-sinks | awk '/muted/ { print $2 }')" == "yes" ]; then
-	echo "SILENCIAT"
-	exit 0
-fi;
-
-# en cas que no estigui mutejat no haurà sortit
-v=$(vol)
-if [ "$v" -gt "100" ]  ; then echo "  $v%";
-elif [ "$v" -ge "50" ] ; then echo " $v%";
-else echo " $v%"; fi;
+_BLOCK_39_
